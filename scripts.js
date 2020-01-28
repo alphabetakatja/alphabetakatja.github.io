972c38f5ba271a7ce43d3b6f93d8c0df55a5aad7
alphabetakatja <47277248+alphabetakatja@users.noreply.github.com>
2020-01-28 13:50:54 +0100

    Add files via upload
    Initial commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// function expression to select elements
+const selectElement = s => document.querySelector(s);
+
+// open the menu on click
+selectElement(".open").addEventListener("click", () => {
+  console.log("i am clicking to open the menu");
+  selectElement(".nav-list").classList.add("active");
+});
+
+// close the menu on click
+selectElement(".close").addEventListener("click", () => {
+  console.log("i am clicking to close the menu");
+  selectElement(".nav-list").classList.remove("active");
+});
